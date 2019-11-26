77fc5d1510f8c8f3fbe03695c996e77f6c27a427
Leonardo Coelho <l.coelho@ecotrace.info>
2019-11-26 18:21:53 -0300

    alteração botão

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,9 @@ $( document ).ready(function() {
         $('.form-step').removeClass( "active" );
         $('.user-infos').addClass( "active" );
     });
+
+    $('#form-prev-btn').on( "click", function() {
+        $('.form-step').removeClass( "active" );
+        $('.tag-cloud').addClass( "active" );
+    });
 });
