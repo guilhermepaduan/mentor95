fb4469fff9bff5ea81255f3ad68ac17206111f97
Your Name <you@example.com>
2019-11-26 23:41:06 -0300

    Home parte

---
+++ app/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Mentor 95</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+    <link rel="stylesheet" href="dist/css/main.min.css">
+    <script src='main.js'></script>
+</head>
+<body>
+
+  <header id="header">
+    <div class="alignment">
+      <figure class="logotipo">
+        <img src="dist/images/logomentor.png" alt="">
+      </figure>
+
+      <nav class="menu">
+        <ul>
+          <li><a href="#">Entrar</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <main id="content">
+    <div class="alignment">
+
+      <div class="banner-destaque">
+        <h1>Descubra seu futuro</h1>
+        <p>Deixe nossos mentores imergir você no mundo da programação e descubra se essa é sua vocação,<br>veja como tudo acontece no setor que mais cresce no mundo.</p>
+
+        <a href="#">Cadastrar</a>
+      </div>
+
+      <div class="home-destaque">
+        <ul>
+          <li>
+            <h2>Quero conhecer uma nova área de programação ou tecnologia</h2>
+            <p>Se você já está imerso no mundo da programação e quer migrar de área ou mudar de tecnologia, nosso mentores também podem te ajudar.</p>
+            <a href="#">Cadastrar</a>
+          </li>
+
+          <li>
+            <h2>Quero ser um mentor</h2>
+            <p>Aqui você pode ser mentor e também receber mentoria, todos somos iguais. Nossos mentores são avaliados pelos seus mentorados e seu perfil ganha visibilidade para novas vagas e oportunidades de emprego.</p>
+            <a href="#">Cadastrar</a>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+  </main>
+
+  <footer id="footer">
+    <div class="alignment">
+      &copy; Feito com muito amor e carinho pelo time Mentor95
+    </div>
+  </footer>
+
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+  <script src='dist/js/main.js'></script>
+</body>
+</html>
